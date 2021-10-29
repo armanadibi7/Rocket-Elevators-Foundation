b5ffb8daebb902d9231bd027cd1b442baf3c509d
Do Minh An Nguyen <minhan145998@gmail.com>
2021-10-29 08:52:21 -0400

    added required files for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "DoMinhAnNguyen"
 set :repo_url, "https://github.com/dominhannguyen/Rocket_Elevators_Information_System"
-
+set :branch, 'main'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
