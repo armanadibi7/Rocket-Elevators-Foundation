7a474512fe401b01f690e3aec1c3e3c6832d82b4
jakeg814 <jakegomez814@hotmail.com>
2021-11-02 17:37:51 -0400

    finished data implementation (for now)

--- db/seeds.rb
+++ db/seeds.rb
@@ -195,54 +195,57 @@ end
     )
 end
 
-# 100.times do
-#     buildings = Building.create!(
+100.times do
+    buildings = Building.create!(
 
-#         admin_name: Faker::Name.name, #=> "Hirthe-Ritchie"
-#         # company_address: address.full, #get randomly from address table
-#         admin_email: Faker::Internet.unique.email, #=> "Tyshawn Johns Sr."
-#         admin_phone_numer: Faker::PhoneNumber.cell_phone, #=> "(186)285-7925"
-#         tech_full_name: Faker::Name.name, #=> "eliza@mann.net"
-#         tech_email: Faker::Internet.unique.email, #=> "Business-focused coherent parallelism"
-#         tech_phone_number: Faker::PhoneNumber.cell_phone, #=> "Tyshawn Johns Sr."
-#         address_id: Faker::Number.between(from: 1, to: 100),
-#         customer_id: Faker::Number.between(from: 1, to: 100) #=> "eliza@mann.net"
-#     )
-# end
+        admin_name: Faker::Name.name, 
+        # company_address: address.full, 
+        admin_email: Faker::Internet.unique.email, 
+        admin_phone_numer: Faker::PhoneNumber.cell_phone, 
+        tech_full_name: Faker::Name.name, 
+        tech_email: Faker::Internet.unique.email, 
+        tech_phone_number: Faker::PhoneNumber.cell_phone, 
+        address_id: Faker::Number.between(from: 1, to: 100),
+        customer_id: Faker::Number.between(from: 1, to: 100) 
+    )
+end
 
-# 100.times do
-#     batteries = Battery.create!(
+100.times do
+    batteries = Battery.create!(
 
-#         battery_type: ["Residential", "Commercial", "Corporate", "Hybrid"].sample,
-#         status: "Active", 
-#         date_of_commissioning: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), #=> #<Date: 2014-09-24>, 
-#         date_of_last_inspection: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), #=> #<Date: 2014-09-24>, 
-#         certificate_of_operations: 'Certified',
-#         information: Faker::Lorem.sentence(word_count: 11),
-#         notes: Faker::Lorem.sentence(word_count: rand(3..12).floor)
-#     )
-# end
+        battery_type: ["Residential", "Commercial", "Corporate", "Hybrid"].sample,
+        status: "Active", 
+        date_of_commissioning: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), 
+        date_of_last_inspection: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), 
+        certificate_of_operations: 'Certified',
+        information: Faker::Lorem.sentence(word_count: 11),
+        notes: Faker::Lorem.sentence(word_count: rand(5..10).floor)
+    )
+end
 
-# 100.times do
-#     columns = Column.create!(
-#         column_type: ['Commercial', 'Residential', 'Corporate',].sample, 
-#         number_of_floor: Faker::Number.decimal_part(digits: 2), #=> "09"
-#         status: Faker::Boolean.boolean, #=> true
-#         information: Faker::Types.complex_rb_hash(number: 1), #=> {user: {first: "bob", last: "marley"}}
-#         notes: Faker::Quote.yoda #=> "Use your feelings, Obi-Wan, and find him you will."
-#     )
-# end
+100.times do
+    columns = Column.create!(
+        column_type: ['Commercial', 'Residential', 'Corporate',].sample, 
+        number_of_floor: Faker::Number.decimal_part(digits: 2), 
+        status: 'Active', #Should be either active or inactive, based off of battery??? -jake
+        information: Faker::Lorem.sentence(word_count: rand(3..12).floor),
+        # information: Faker::Types.complex_rb_hash(number: 1), alternative info -jake
+        notes: Faker::Quote.yoda,
+        battery_id: Faker::Number.between(from: 1, to: 100)
+    )
+end
 
-# 100.times do
-#     elevators = Elevator.create!(
-#         serial_number: Faker::Number.number(digits: 10), #=> 1968353479
-#         model: ['Standard', 'Premium', 'Excelium'].sample, 
-#         elevator_type: ['Commercial', 'Residential', 'Corporate',].sample, 
-#         status: Faker::Boolean.boolean, #=> true
-#         date_of_commissioning: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), #=> #<Date: 2014-09-24>
-#         date_of_last_inspection: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), #=> #<Date: 2014-09-24>
-#         notes: Faker::Quote.yoda #=> "Use your feelings, Obi-Wan, and find him you will."
-#     )
-# end
+100.times do
+    elevators = Elevator.create!(
+        serial_number: Faker::Number.number(digits: 10),
+        model: ['Standard', 'Premium', 'Excelium'].sample, 
+        elevator_type: ['Commercial', 'Residential', 'Corporate',].sample, 
+        status: ["Active", "Inactive"].sample,
+        date_of_commissioning: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), 
+        date_of_last_inspection: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), 
+        notes: Faker::Quote.yoda,
+        column_id: Faker::Number.between(from: 1, to: 100)
+    )
+end
 
 puts "Seeding Done!"
