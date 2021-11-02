be0d9514b54351b3e143cddfa02cf9767c0c2790
Do Minh An Nguyen <minhan145998@gmail.com>
2021-11-02 16:28:39 -0400

    Removed first name last name in user table

--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,6 @@ ActiveRecord::Schema.define(version: 2021_11_02_194511) do
 
   create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "company_name"
-    t.string "company_address"
     t.string "company_contact_name"
     t.string "contact_phone"
     t.string "contact_email"
@@ -153,9 +152,6 @@ ActiveRecord::Schema.define(version: 2021_11_02_194511) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
     t.boolean "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
