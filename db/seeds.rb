47f3886f8e28b619c61f3ea9b5798f36ac10f4ac
jakeg814 <jakegomez814@hotmail.com>
2021-11-02 16:19:19 -0400

    before pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -165,7 +165,7 @@ require 'faker'
 
 100.times do
     address = Addressy::US.fetch(10).first
-    addresses = Address.new(
+    addresses = Address.create!(
         
         address_type: ["House", "Apartment", "Commercial", "Corporate", "Hybrid"].sample,
         status: ["Active", "Inactive"].sample,
@@ -177,14 +177,13 @@ require 'faker'
         country: "USA",
         notes: Faker::Lorem.sentence(word_count: rand(3..9).floor)
     )
-    addresses.save
 end
 
 100.times do
-    customers = Customer.new(
+    customers = Customer.create!(
 
         company_name: Faker::Company.name, #=> "Hirthe-Ritchie"
-        #company_address: addressy.full, #link to real address with addressy
+        # company_address: address.full, #get randomly from address table
         company_contact_name: Faker::Name.name , #=> "Tyshawn Johns Sr."
         contact_phone: Faker::PhoneNumber.cell_phone, #=> "(186)285-7925"
         contact_email: Faker::Internet.unique.email, #=> "eliza@mann.net"
@@ -192,12 +191,12 @@ end
         service_tech_name: Faker::Name.name , #=> "Tyshawn Johns Sr."
         service_tech_phone: Faker::PhoneNumber.cell_phone, #=> "(186)285-7925"
         service_tech_email: Faker::Internet.unique.email #=> "eliza@mann.net"
+        address_id: 
     )
-    customers.save
 end
 
 100.times do
-    batteries = Battery.new(
+    batteries = Battery.create!(
 
         battery_type: ["Residential", "Commercial", "Corporate", "Hybrid"].sample,
         status: "Active", 
@@ -207,22 +206,20 @@ end
         information: Faker::Lorem.sentence(word_count: 11),
         notes: Faker::Lorem.sentence(word_count: rand(3..12).floor)
     )
-    batteries.save
 end
 
 100.times do
-    columns = Column.new(
+    columns = Column.create!(
         column_type: ['Commercial', 'Residential', 'Corporate',].sample, 
         number_of_floor: Faker::Number.decimal_part(digits: 2), #=> "09"
         status: Faker::Boolean.boolean, #=> true
         information: Faker::Types.complex_rb_hash(number: 1), #=> {user: {first: "bob", last: "marley"}}
         notes: Faker::Quote.yoda #=> "Use your feelings, Obi-Wan, and find him you will."
     )
-    columns.save
 end
 
 100.times do
-    elevators = Elevator.new(
+    elevators = Elevator.create!(
         serial_number: Faker::Number.number(digits: 10), #=> 1968353479
         model: ['Standard', 'Premium', 'Excelium'].sample, 
         elevator_type: ['Commercial', 'Residential', 'Corporate',].sample, 
@@ -231,7 +228,6 @@ end
         date_of_last_inspection: Faker::Date.between(from: '2017-09-23', to: '2021-09-25'), #=> #<Date: 2014-09-24>
         notes: Faker::Quote.yoda #=> "Use your feelings, Obi-Wan, and find him you will."
     )
-    elevators.save
 end
 
 puts "Seeding Done!"
