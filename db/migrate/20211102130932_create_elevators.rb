8f6f8af00ecdced5f566e8e23068dba8b685320d
Do Minh An Nguyen <minhan145998@gmail.com>
2021-11-02 13:20:10 -0400

    Changed column name from type to column_type, elevator_type, battery_type

--- db/migrate/20211102130932_create_elevators.rb
+++ db/migrate/20211102130932_create_elevators.rb
@@ -3,7 +3,7 @@ class CreateElevators < ActiveRecord::Migration[5.2]
     create_table :elevators do |t|
       t.string :serial_number
       t.string :model
-      t.string :type
+      t.string :elevator_type
       t.string :status
       t.date :date_of_commissioning
       t.date :date_of_last_inspection
--- db/migrate/20211102131531_create_batteries.rb
+++ db/migrate/20211102131531_create_batteries.rb
@@ -1,7 +1,7 @@
 class CreateBatteries < ActiveRecord::Migration[5.2]
   def change
     create_table :batteries do |t|
-      t.string :type
+      t.string :battery_type
       t.string :status
       t.date :date_of_commissioning
       t.date :date_of_last_inspection
