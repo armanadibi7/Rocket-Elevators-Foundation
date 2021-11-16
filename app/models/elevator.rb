221f34b2a32321b5a4670b875be933735e973b8f
Azriely <dandurand14m@gmail.com>
2021-11-16 13:23:58 -0500

    twilio functional

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -9,22 +9,27 @@ class Elevator < ApplicationRecord
     # TWILIO
     def sms_tech
         # Download the helper library from https://www.twilio.com/docs/ruby/install
+        require 'rubygems'
         require 'twilio-ruby'
 
         # Find your Account SID and Auth Token at twilio.com/console
         # and set the environment variables. See http://twil.io/secure
-        account_sid = ENV['twilio_acct_id']
-        auth_token = ENV['twilio_auth']
+        account_sid = ENV["twilio_acct_id"]
+        auth_token = ENV["twilio_auth"]
         @client = Twilio::REST::Client.new(account_sid, auth_token)
 
         from = '+17179958791' # Your Twilio number
         to = '+15142588609' # Your mobile phone number
-
-        client.messages.create(
-        from: from,
-        to: to,
-        body: "Hey friend!"
-        )
+         
+        message = @client.messages.create( 
+                                     body: "The Elevator with ID: '#{self.id}' in building ID: '#{self.column.battery.building.id}' status has changed from '#{self.status_was}' to '#{self.status}'",  
+                                     messaging_service_sid: ENV["messaging_sid"],      
+                                     to: to
+                                   ) 
+         
+        puts message.sid
+        
     end
 
 end
+
