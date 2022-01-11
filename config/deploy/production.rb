e3d457e31978a1561ffd2c3e793a96cb05fec041
armanadibi7 <armanadibi7@gmail.com>
2022-01-11 11:55:25 -0500

    Deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "54.146.245.79", user: "rails", roles: %w{app db web}
+server "3.88.249.55", user: "rails", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 app = ENV['APP']
@@ -27,9 +27,9 @@ set :linked_files, %w{config/application.yml config/database.yml }
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{rails@54.146.245.79}
-role :web, %w{rails@54.146.245.79}
-role :db,  %w{rails@54.146.245.79}
+role :app, %w{rails@3.88.249.55}
+role :web, %w{rails@3.88.249.55}
+role :db,  %w{rails@3.88.249.55}
 
 
 
